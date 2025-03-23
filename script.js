c64575aebe343f0285698b47431e80a6a8174157
AzimS63190 <azimcontact25@gmail.com>
2025-03-24 01:48:26 +0800

    Update 5 Section
    Navbar Section, Home Section, About Me Section, Skills Section and Contact Section

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Message Sent! We'll get back to you soon.");
-});
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message sent successfully!");
+  });
+  
